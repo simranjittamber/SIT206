97312d53ec489679d3497a5ee78466fb9ef4b074
SIMRANJIT SINGH <wno@10-150-45-233.dhcp-m.deakin.edu.au>
2018-04-17 16:16:13 +1000

    1st check in

---
+++ week5/week5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  week5
+//
+//  Created by SIMRANJIT SINGH on 17/4/18.
+//  Copyright © 2018 SIMRANJIT SINGH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
